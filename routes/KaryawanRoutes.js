b185596c96b4ced6b288d6af2b5894ba9da9b56f
salsabilavebi <salsabilavebinatasya19@gmail.com>
2022-12-30 15:16:40 +0700

    deploy

--- routes/KaryawanRoutes.js
+++ routes/KaryawanRoutes.js
@@ -9,10 +9,12 @@ import {
 import {verifyUser, adminOnly} from "../middleware/AuthUser.js";
 const router = express.Router();
 
+
 router.get('/karyawans',verifyUser, adminOnly, getKaryawans);
 router.get('/karyawans/:id',verifyUser, adminOnly, getKaryawanById);
 router.post('/karyawans',verifyUser, adminOnly, saveKaryawan);
 router.patch('/karyawans/:id',verifyUser, adminOnly, updateKaryawan);
 router.delete('/karyawans/:id',verifyUser, adminOnly, deleteKaryawan);
+
 
 export default router;
